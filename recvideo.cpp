84daea7b48a4d57367f6560a0579d29b7ce5de9f
Fercho <wfpinedar@gmail.com>
2013-12-15 11:00:08 -0500

    Se realiza la corección para que el usuario pueda realizar la selección del direcctorio donde desea guardar el video

--- recvideo.cpp
+++ recvideo.cpp
@@ -2,16 +2,21 @@
 #include "ui_recvideo.h"
 #include "string.h"
 #include "QMessageBox"
+#include "QFileDialog"
 
 recvideo::recvideo(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::recvideo)
 {
+
     ui->setupUi(this);
     ui->ancho->setText("1024");
     ui->alto->setText("768");
     ui->framerate->setText("20");
-    ui->namevideo->setText("video.avi");
+    connect(ui->actionSalir, SIGNAL(triggered()), qApp, SLOT(quit()));
+    connect(ui->actionGuardar, SIGNAL(triggered()), this, SLOT(on_namevideo_selectionChanged()));
+    QString fileName;
+    ui->namevideo->setText(fileName);
 }
 
 recvideo::~recvideo()
@@ -21,9 +26,10 @@ recvideo::~recvideo()
 
 void recvideo::on_pushButton_clicked()
 {
+
     QString uno = "ffmpeg -r ";
     QString dos = " -s ";
-    QString tres = " -f x11grab -i :0.0 -vcodec msmpeg4v2 -qscale 1 /home/$USER/VÃ­deos/";
+    QString tres = " -f x11grab -i :0.0 -vcodec msmpeg4v2 -qscale 1 ";
     QString fin = " &";
     QString nombre = ui->namevideo->text();
     QString framerate = ui->framerate->text();
@@ -42,5 +48,24 @@ void recvideo::on_pushButton_2_clicked()
     QMessageBox msgBox;
     msgBox.setText("Esta saliendo para continuar puse OK");
     msgBox.exec();
+    system("pkill -9 ffmpeg");
     close();
+}
+
+
+void recvideo::on_namevideo_selectionChanged()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Asigne ubicación y nombre Para guardar el vídeo"), ".avi",
+        tr("Ví­deo File (*.avi);;All Files (*. *.h)"));
+
+    ui->namevideo->setText(fileName);
+}
+
+
+void recvideo::on_namevideo_returnPressed()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), ".avi",
+        tr("Ví­deo File (*.avi);;All Files (*. *.h)"));
+
+    ui->namevideo->setText(fileName);
 }
--- recvideo.h
+++ recvideo.h
@@ -20,6 +20,10 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_namevideo_selectionChanged();
+
+    void on_namevideo_returnPressed();
+
 private:
     Ui::recvideo *ui;
 };
